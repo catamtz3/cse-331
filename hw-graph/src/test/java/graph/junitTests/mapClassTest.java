e5ff661179575be6ec742240a4c46b80482429d2
catamtz3 <catamtz3@cs.washington.edu>
2021-04-29 22:14:06 -0700

    commit

--- hw-graph/src/test/java/graph/junitTests/mapClassTest.java
+++ hw-graph/src/test/java/graph/junitTests/mapClassTest.java
@@ -2,9 +2,6 @@ package graph.junitTests;
 
 import graph.mapClass;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.*;
 
 public class mapClassTest {
